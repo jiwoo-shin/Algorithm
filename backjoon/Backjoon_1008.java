dcbd4fbcee0a6ef0684126e4bb5c51253eeb639f
jiwooShin <jiwoo_shin_@naver.com>
2019-01-31 23:30:32 +0900

    코드그라운드 1008번 (#10)

---
+++ backjoon/Backjoon_1008.java
@@ -0,0 +1,22 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class Backjoon_1008 extends Solution {
+	/*
+	 * 요약 : A/B 구하기
+	 * */
+	@Override
+	public void solution() {
+		Scanner sc = new Scanner(System.in);
+		//double A = sc.nextDouble();
+		//double B = sc.nextDouble();
+		//System.out.println(A/B);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		System.out.println((double)A/B);
+		
+	}
+
+}
