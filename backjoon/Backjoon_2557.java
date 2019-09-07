fc81fc7fa35b00e030d86be84fa8131731bc0fc0
jiwooShin <jiwoo_shin_@naver.com>
2019-09-08 00:36:01 +0900

    백준앙ㄹ고리즘 2557번 (#17)

---
+++ backjoon/Backjoon_2557.java
@@ -0,0 +1,12 @@
+package backjoon;
+
+public class Backjoon_2557 extends Solution {
+
+	@Override
+	public void solution() {
+		
+		System.out.println("Hello World!");
+	
+	}
+
+}
--- backjoon/Main.java
+++ backjoon/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		Solution s = new Backjoon_10998();
+		Solution s = new Backjoon_2557();
 		s.solution();
 	}
 
