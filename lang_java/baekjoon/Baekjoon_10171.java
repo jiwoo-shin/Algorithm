cb0592ee09f9ac41c495a3ed46d8ccb914883a12
jiwooShin <jiwoo_shin_@naver.com>
2019-09-28 00:03:37 +0900

    백준알고리즘 10171번(#24)

---
+++ lang_java/baekjoon/Baekjoon_10171.java
@@ -0,0 +1,13 @@
+package lang_java.baekjoon;
+
+
+public class Baekjoon_10171 extends Solution {
+	
+	@Override
+	public void solution() {
+		System.out.print("\\    /\\\n" + 
+				" )  ( ')\n" + 
+				"(  /  )\n" + 
+				" \\(__)|");
+	}
+}
--- lang_java/baekjoon/Main.java
+++ lang_java/baekjoon/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		Solution s = new Baekjoon_10869();
+		Solution s = new Baekjoon_10171();
 		s.solution();
 	}
 
