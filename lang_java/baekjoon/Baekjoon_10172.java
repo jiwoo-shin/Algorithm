22e36403e3c00c8e7b43f785014f79ec1b057ba1
jiwooShin <jiwoo_shin_@naver.com>
2019-09-28 23:55:22 +0900

    백준알고리즘 10172번 (#25)

---
+++ lang_java/baekjoon/Baekjoon_10172.java
@@ -0,0 +1,14 @@
+package lang_java.baekjoon;
+
+
+public class Baekjoon_10172 extends Solution {
+	
+	@Override
+	public void solution() {
+		System.out.print("|\\_/|\n" + 
+				"|q p|   /}\n" + 
+				"( 0 )\"\"\"\\\n" + 
+				"|\"^\"`    |\n" + 
+				"||_/=\\\\__|");
+	}
+}
--- lang_java/baekjoon/Main.java
+++ lang_java/baekjoon/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		Solution s = new Baekjoon_10171();
+		Solution s = new Baekjoon_10172();
 		s.solution();
 	}
 
