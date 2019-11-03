5bad3bdacc7019e7015a82d0b5dfb77ee38bef18
jiwooShin <jiwoo_shin_@naver.com>
2019-11-03 23:47:24 +0900

    백준알고리즘 10817번 (#32)

---
+++ lang_java/baekjoon/Baekjoon_10817.java
@@ -0,0 +1,35 @@
+package lang_java.baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon_10817 extends Solution {
+	
+	@Override
+	public void solution() {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		
+		if(A < B) {
+			if(C < A) { // B > A > C
+				System.out.println(A);
+			} else if (C < B){ //B > C >A
+				System.out.println(C);
+			} else { // C > B > A
+				System.out.println(B);
+			}
+		} else { // A >= B
+			if(C > A) { // C > A > B
+				System.out.println(A);
+			} else if (C < B){ // A > B > C
+				System.out.println(B);
+			} else { // A > C > B
+				System.out.println(C);
+			}
+		}
+		
+		
+	}
+}
