92600128133a9002c8f323f7d8c0b9783af4cd33
jiwooShin <jiwoo_shin_@naver.com>
2019-11-05 23:13:20 +0900

    백준알고리즘 15665번 (#31)

--- lang_java/baekjoon/Main.java
+++ lang_java/baekjoon/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		
-		Solution s = new Baekjoon_15664();
+		Solution s = new Baekjoon_15665();
 		s.solution();
 	}
 
