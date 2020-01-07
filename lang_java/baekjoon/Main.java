2677a110f7dfcf60af8500dc80caa893dd6b499c
jiwooShin <jiwoo_shin_@naver.com>
2020-01-08 00:07:53 +0900

    백준알고리즘 11726번 (#35)

--- lang_java/baekjoon/Main.java
+++ lang_java/baekjoon/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		
-		Solution s = new Baekjoon_2206();
+		Solution s = new Baekjoon_11726();
 		s.solution();
 	}
 
