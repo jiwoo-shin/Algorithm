ab0e3045f831019fc084df0842b22474afaf60fc
jiwooShin <jiwoo_shin_@naver.com>
2020-01-08 23:22:07 +0900

    백준알고리즘 9095번 (#35)

--- lang_java/baekjoon/Baekjoon_9095.java
+++ lang_java/baekjoon/Baekjoon_9095.java
@@ -61,6 +61,33 @@ public class Baekjoon_9095 extends Solution {
 		} else return 0; //current가 goal보다 커진 경우이므로 답이 아님
 	}
 	
+	// dp (bottom-up)
+	static int bottomUp(int[] memo, int n) {
+		memo[0] = 1;
+		if(n > 1) memo[1] = 2;
+		if(n > 2) memo[2] = 4;
+		// f(n) = f(n-1) 로 만든 후 1을 더하는 경우 + f(n-2)로 만든 후 2를 더하는 경우 + f(n-3)으로 더한 후 3을 더하는 경우
+		for(int i = 3; i < n; i++) {
+			memo[i] = memo[i-1] + memo[i-2] + memo[i-3];
+		}
+		return memo[n-1];
+	}
+	
+	// dp (top-down)
+	static int topDown(int[] memo, int n) {
+		int index = n-1;
+		if(memo[index] == 0) {
+			if(index == 0) memo[index] = 1;
+			else if(index == 1) memo[index] = 2;
+			else if(index == 2) memo[index] = 4;
+			else {
+				memo[index] = topDown(memo, n-1) + topDown(memo, n-2) + topDown(memo, n-3);
+			}
+		}
+		
+		return memo[index];
+	}
+	
 	@Override
 	public void solution() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -72,7 +99,9 @@ public class Baekjoon_9095 extends Solution {
 		for(int i = 0; i < T; i++) {
 			st = new StringTokenizer(br.readLine());
 			int n = Integer.parseInt(st.nextToken());
-			sb.append(sum(0, n)).append('\n');
+			sb.append(bottomUp(new int[n], n)).append('\n');
+			sb.append(topDown(new int[n], n)).append('\n');
+			// sb.append(sum(0, n)).append('\n');
 		}
 		System.out.println(sb.toString());
 		
--- lang_java/baekjoon/Main.java
+++ lang_java/baekjoon/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		
-		Solution s = new Baekjoon_11727();
+		Solution s = new Baekjoon_9095();
 		s.solution();
 	}
 
