c04a494d4f4a094d6718515867e2efe19714ac09
jiwooShin <jiwoo_shin_@naver.com>
2020-01-12 23:24:15 +0900

    백준알고리즘 2193번 (#31)

--- lang_java/baekjoon/Main.java
+++ lang_java/baekjoon/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		
-		Solution s = new Baekjoon_11057();
+		Solution s = new Baekjoon_2193();
 		s.solution();
 	}
 
