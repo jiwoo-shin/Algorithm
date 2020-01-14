43f0a57421a7d4943406bdd34b1ae2f9711804e8
jiwooShin <jiwoo_shin_@naver.com>
2020-01-15 00:04:34 +0900

    백준알고리즘 9465번 (#35)

--- lang_java/baekjoon/Main.java
+++ lang_java/baekjoon/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		
-		Solution s = new Baekjoon_2193();
+		Solution s = new Baekjoon_9465();
 		s.solution();
 	}
 
