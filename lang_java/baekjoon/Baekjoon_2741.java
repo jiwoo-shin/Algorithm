bafd605b28a0df9bb7c7f2d179f02025ae6b6119
jiwooShin <jiwoo_shin_@naver.com>
2020-02-09 23:02:00 +0900

    백준알고리즘 2741번 (#33)

---
+++ lang_java/baekjoon/Baekjoon_2741.java
@@ -0,0 +1,19 @@
+package lang_java.baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon_2741 extends Solution {
+	
+	@Override
+	public void solution() {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		StringBuffer buffer = new StringBuffer();
+		for(int i = 1; i < N+1; i++) {
+			buffer.append(i).append('\n');
+		}
+		
+		System.out.print(buffer);
+	}
+}
