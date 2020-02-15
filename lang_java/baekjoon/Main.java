de169004d6a02087eef8e314c252bbd4001be719
jiwooShin <jiwoo_shin_@naver.com>
2020-02-15 23:41:44 +0900

    백준알고리즘 6064번 (#19)

--- lang_java/baekjoon/Main.java
+++ lang_java/baekjoon/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		
-		Solution s = new Baekjoon_1107();
+		Solution s = new Baekjoon_6064();
 		s.solution();
 	}
 
