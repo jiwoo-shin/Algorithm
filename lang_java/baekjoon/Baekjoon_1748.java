02c16be24593047f60d321c8c87cde186253fc1d
jiwooShin <jiwoo_shin_@naver.com>
2020-02-28 01:01:31 +0900

    백준 알고리즘 1339번 (#19)

--- lang_java/baekjoon/Baekjoon_1748.java
+++ lang_java/baekjoon/Baekjoon_1748.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Baekjoon_1718 extends Solution {
+public class Baekjoon_1748 extends Solution {
 	
 	@Override
 	public void solution() throws IOException {
--- lang_java/baekjoon/Main.java
+++ lang_java/baekjoon/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		
-		Solution s = new Baekjoon_2529();
+		Solution s = new Baekjoon_1339();
 		s.solution();
 	}
 
