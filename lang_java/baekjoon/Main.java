ca1e75bba25c162867386de980e710ef09d82927
jiwooShin <jiwoo_shin_@naver.com>
2020-03-05 22:32:20 +0900

    백준알고리즘 9663번 (#19)

--- lang_java/baekjoon/Main.java
+++ lang_java/baekjoon/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		
-		Solution s = new Baekjoon_1248();
+		Solution s = new Baekjoon_9663();
 		s.solution();
 	}
 
