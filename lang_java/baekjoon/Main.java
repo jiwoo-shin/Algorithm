75feb2ba90d24dba1942c7fa26c00e7a99d91723
jiwooShin <jiwoo_shin_@naver.com>
2020-03-09 22:41:38 +0900

    백준알고리즘 1987번 (#19)

--- lang_java/baekjoon/Main.java
+++ lang_java/baekjoon/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		
-		Solution s = new Baekjoon_2580();
+		Solution s = new Baekjoon_1987();
 		s.solution();
 	}
 
