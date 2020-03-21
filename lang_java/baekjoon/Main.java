c2c0a01c642ef762e20f31296c65739d6aa7eacf
jiwooShin <jiwoo_shin_@naver.com>
2020-03-21 22:33:16 +0900

    백준알고리즘 12100번 (#19)

--- lang_java/baekjoon/Main.java
+++ lang_java/baekjoon/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		
-		Solution s = new Baekjoon_13460();
+		Solution s = new Baekjoon_12100();
 		s.solution();
 	}
 
