499f2b2ede2c9a54f82abeddebd9ef34ef147519
jiwooShin <jiwoo_shin_@naver.com>
2020-04-03 23:02:41 +0900

    BOJ 9019번 (#34)

--- lang_java/baekjoon/Main.java
+++ lang_java/baekjoon/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		
-		Solution s = new Baekjoon_13913();
+		Solution s = new Baekjoon_9019();
 		s.solution();
 	}
 
