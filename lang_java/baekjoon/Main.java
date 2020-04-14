0f38e15e19ec97c8018808cfa3854993dbcdcc2e
jiwooShin <jiwoo_shin_@naver.com>
2020-04-14 23:45:12 +0900

    BOJ 9376번 (#34)

--- lang_java/baekjoon/Main.java
+++ lang_java/baekjoon/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		
-		Solution s = new Baekjoon_9376();
+		Solution s = new Baekjoon_9328();
 		s.solution();
 	}
 
