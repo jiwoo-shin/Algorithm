20c7faac48de295229255fdbf6b83bdf229c4164
jiwooShin <jiwoo_shin_@naver.com>
2020-05-06 22:42:48 +0900

    BOJ 11066번 (#35)

--- lang_java/baekjoon/Solution.java
+++ lang_java/baekjoon/Solution.java
@@ -4,4 +4,15 @@ import java.io.IOException;
 
 public abstract class Solution {
 	public abstract void solution() throws IOException;
+
+	
+	static void print(int[][] array) {
+		System.out.println();
+		for(int i = 0; i < array.length; i++) {
+			for(int j = 0; j < array[i].length; j++) {
+				System.out.print(array[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
 }
