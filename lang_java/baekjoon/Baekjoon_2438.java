3b959229aaf5529c37c9ee375f97f4d00c9222d3
jiwooShin <jiwoo_shin_@naver.com>
2020-06-21 22:20:51 +0900

    BOJ 2438번 (#33)

---
+++ lang_java/baekjoon/Baekjoon_2438.java
@@ -0,0 +1,26 @@
+package lang_java.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon_2438 extends Solution {
+
+	@Override
+	public void solution() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		printStar(N);
+		
+	}
+	static void printStar(int N) {
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j <= i; j++) {
+				sb.append('*');
+			}
+			sb.append('\n');
+		}
+		System.out.print(sb.toString());
+	}
+}
--- lang_java/baekjoon/Main.java
+++ lang_java/baekjoon/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		
-		Solution s = new Baekjoon_15661();
+		Solution s = new Baekjoon_2438();
 		s.solution();
 	}
 
