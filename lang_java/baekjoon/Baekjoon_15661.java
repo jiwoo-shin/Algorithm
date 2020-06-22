a015cdbb6b359f8b51c45dc0d3b07249f4197742
jiwooShin <jiwoo_shin_@naver.com>
2020-06-22 22:50:48 +0900

    BOJ 15661번 (#19)

--- lang_java/baekjoon/Baekjoon_15661.java
+++ lang_java/baekjoon/Baekjoon_15661.java
@@ -20,25 +20,39 @@ public class Baekjoon_15661 extends Solution {
 			st = new StringTokenizer(br.readLine());
 			for(int j = 0; j < N; j++) S[i][j] = Integer.parseInt(st.nextToken());
 		}
-		System.out.println(dfs(new int[N], new int[N], 0, 0, 0));
+		System.out.println(search());
+		//System.out.println(dfs(new int[N], new int[N], 0, 0, 0));
 		
 	}
 	static int search() {
 		int answer = 0;
-		int max = 1 << N;
-		for(int i = 1; i <= max; i++) { // 한명도 없는 경우는 없다.
-			
+		int max = (1 << N)-1;
+		int[] score = new int[max]; // 속한 팀원이 i 인 경우 얻을 수 있는 점수
+		for(int i = 1; i < max; i++) { // 한명도 없는 경우는 없다. 0... 이거나 1...인 경우는 제외
+			int new_student = -1;
+			for(int j = 0; j < N; j++) { //j 번 학생이 해당 팀에 속하는지 확인
+				int check = i & (1 << j);
+				if(check != 0) { // j 번째 학생이 팀에 포함되는 경우 (==1 아님.. 1이 아닐수도 있음 2나.. 4나..)
+					if(new_student == -1) { // 이전에 구했던 팀에서 새로운 학생이 들어오는 경우 (왼쪽부터 첫번째 1이 나오면 해당 학생은 새로운 학생)
+						int prev_team = i &(~check); // 팀 i 에서 가장 좌측의 1만 제거한 상태 (i 보다 작으므로 이전에 구한 점수가 있음)
+						score[i] = score[prev_team];
+						new_student = j; //가장 좌측 1이 새로운 학생
+					} else { // 새로 들어온 학생과 이전 팀에서의 학생들끼리 조합으로 점수를 구함
+						score[i] += S[new_student][j] + S[j][new_student];
+					}
+				} 
+			}
 		}
+		answer = getDifference(score, max, (max+1)/2);
+		print(score);
 		return answer;
 	}
-	static int getDifference(int team) {
-		int score = 0;
-		int start = 0;
-		int link = 0;
-		for(int i = 0; i < N; i++) {
-			int now = (team >> i) & 1; // 1이면 start, 0이면 link
+	static int getDifference(int[] score, int max, int half) {
+		int difference = Integer.MAX_VALUE;
+		for(int i = 1; i <= half; i++) { // 대칭이므로 반만 한다. 
+			difference = Math.min(difference, Math.abs(score[max-i] - score[i])); // i랑 i 반대랑 (1011, 0100) 차를 구함
 		}
-		return score;
+		return difference;
 	}
 	static int dfs(int[] start, int[] link, int start_index, int link_index, int now) {
 		if(now == N) { 
--- lang_java/baekjoon/Main.java
+++ lang_java/baekjoon/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		
-		Solution s = new Baekjoon_2438();
+		Solution s = new Baekjoon_15661();
 		s.solution();
 	}
 
