c3fca6a985b22f3c770dd152a8fe689420d8e108
jiwoo-shin <jiwoo_shin_@naver.com>
2021-08-24 19:58:34 +0900

    CI Ch01-Q02 (#40)

--- lang_java/src/codingInterview/CH01.java
+++ lang_java/src/codingInterview/CH01.java
@@ -11,7 +11,8 @@ public class CH01 extends Solution {
 	@Override
 	public void solution() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println(q1(br.readLine()));
+		// System.out.println(q1(br.readLine()));
+		System.out.println(q2(br.readLine(), br.readLine()));
 	}
 
 	// 문자 중복 여부 확인
@@ -25,5 +26,21 @@ public class CH01 extends Solution {
 			use[str.charAt(i)] = true;
 		}
 		return false;
+	}
+	
+	// 순열 여부 확인
+	static boolean q2(String str1, String str2) {
+		if(str1.length() != str2.length()) return false;
+		int[] str1Use = new int[128], str2Use = new int[128]; //
+		// 이렇게 안하고 str1 사용만 저장, 확인 시에 str2 문자를 빼는 것으로 확인 가능하다.
+		for(int i = 0; i < str1.length(); i++) {
+			str1Use[str1.charAt(i)]++;
+			str2Use[str2.charAt(i)]++;
+		}
+		for(int i = 0; i < str1.length(); i++) {
+			int tmp = str1.charAt(i);
+			if(str1Use[tmp] != str2Use[tmp]) return false;
+		}
+		return true;
 	}
 }
